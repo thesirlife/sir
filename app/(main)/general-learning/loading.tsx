6515c34fa6b9b9bcfeb4de8aa1ae09e6bfc26daa
Gavin <gavin.a.gregory@gmail.com>
2024-05-29 19:21:12 -0700

    remove loading copmonent casuing hydration error

--- app/(main)/general-learning/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@mui/material";
-
-export default function Loading() {
-  return (
-    <div className="h-[900px]">
-      <CircularProgress />
-    </div>
-  );
-}
