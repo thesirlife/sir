33328991bde62b0b7dc93b8fec73b2cc6e3a9105
Gavin <gavin.a.gregory@gmail.com>
2024-06-03 13:42:53 -0700

    tidy up auth component

--- auth.ts
+++ auth.ts
@@ -5,19 +5,10 @@ export const authOptions: NextAuthConfig = {
   pages: {
     signIn: "/signin",
     signOut: "/auth/signout",
-    error: "/auth/error", // Error code passed in query string as ?error=
   },
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "SIR",
-
-      // The credentials are used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-
       async authorize(credentials, req): Promise<User | null> {
         try {
           const res = await fetch(
@@ -35,13 +26,8 @@ export const authOptions: NextAuthConfig = {
           }
 
           const parsedResponse = await res.json();
-
-          // accessing the jwt returned by server
           const jwt = parsedResponse.access_token;
 
-          // You can make more request to get other information about the user eg. Profile details
-
-          // return user credentials together with jwt
           return {
             ...credentials,
             jwt,
