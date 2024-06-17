ffa4345f8232887726b51a7d5c859a0b9df6df92
Gavin <gavin.a.gregory@gmail.com>
2024-06-17 17:12:30 -0500

    add no cache option back

--- app/data/getUserById.ts
+++ app/data/getUserById.ts
@@ -3,7 +3,9 @@ import { User } from "../types/user/types";
 const getUserById = async (id: number): Promise<User> => {
   const data = await fetch(
     `${process.env.NEXT_PUBLIC_WPREST_ENDPOINT}/users/${id}`,
-    {}
+    {
+      cache: "no-cache",
+    }
   );
   return data.json();
 };
