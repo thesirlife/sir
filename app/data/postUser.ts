835bcaa1fca01a697f36b7ff001ad82e9fa8bf36
Kelly Bleck <kellydbleck@gmail.com>
2024-06-17 17:36:21 -0600

    Add initial JWT token authentication for creation of user

--- app/data/postUser.ts
+++ app/data/postUser.ts
@@ -12,16 +12,60 @@ type PostUserReturn = User & {
   data: string;
 } & Error;
 
+type AuthResponse = {
+  success: boolean;
+  statusCode: number;
+  code: string;
+  message: string;
+  data: {
+    token: string;
+    id: number;
+    email: string;
+    nicename: string;
+    firstName: string;
+    lastName: string;
+    displayName: string;
+  };
+};
+
 export const postUser = async ({
   username,
   email,
   password,
 }: PostUserProps): Promise<PostUserReturn> => {
+	// @TODO :: Create generic auth user that we can delete/change password of at anytime
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_WPAUTH_ENDPOINT}/token`,
+		{
+			method: "POST",
+			body: JSON.stringify({
+				username: 'kelly@edgesfirst.co',
+				password: process.env.WP_PASSWORD,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
+
+	if (!res.ok) {
+		// @TODO :: Type is not right here
+		return null;
+	}
+
+	const parsedResponse: AuthResponse = await res.json();
+	const jwt = parsedResponse.data.token;
+
+	// @TODO :: Password is viewable as string text in Network tab... figure out how to encrypt
   const data = await fetch(
     `${process.env.NEXT_PUBLIC_WPREST_ENDPOINT}/users?username=${email}&email=${email}&password=${password}&first_name=${username}`,
     {
       method: "POST",
-    }
+			headers: {
+				"Authorization": `Bearer ${jwt}`,
+			},
+    },
   );
-  return await data.json();
+  const result = await data.json();
+	return result;
 };
