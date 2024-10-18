1b663bd7f4a5799a1673795088e81ea5b50c1cf4
Ted Poatsy <ted@crafted.solutions>
2024-10-18 09:33:28 -0400

    Separate pendo instatiation from initialization, pass in env variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,18 @@ export default async function RootLayout({
               strategy="beforeInteractive"
             />
             <HubspotTracking session={session} />
-            <PendoScript session={session} />
+            <Script id="pendo-script" strategy="afterInteractive">
+              {`
+								(function(apiKey){
+										(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+										v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+												o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+												y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+												z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+                        })('${process.env.PENDO_API_KEY}');
+							`}
+            </Script>
+            <PendoScript session={session} pendoAccountId={process.env.PENDO_ACCOUNT_ID} />
           </body>
           <GoogleAnalytics gaId="G-L7T5ZQH0G5" />
         </html>
